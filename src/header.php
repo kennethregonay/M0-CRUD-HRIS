6940d5b6e79c88024955f6344257508526a24179
Netkee <kenneth.regonay@gmail.com>
2021-01-08 00:32:41 +0800

    added the style in the header.php

--- src/header.php
+++ src/header.php
@@ -1,12 +1,21 @@
+<?php
+    require_once ('backend.php');
+  $query = "SELECT * FROM employee";
+
+$result_set = $connection->query($query);
+
+?>
 <!DOCTYPE html>
 <!-- navigation bar-->
+     <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/4752a58f75.js" crossorigin="anonymous"></script>
 <nav>
     <div class="logo">
         <a href="index.php"><h4><i class="fas fa-fingerprint"></i>HRIS</h4></a>
     </div>
     <ul class="nav-links">
           <li>
-            <a href="#" class="href"><i class="fas fa-address-card"></i>EMPLOYEE</a>
+            <a href="employee.php" class="href"><i class="fas fa-address-card"></i>EMPLOYEE</a>
         </li>
          <li>
             <a href="#" class="href"><i class="far fa-id-badge"></i>POSITION</a>
