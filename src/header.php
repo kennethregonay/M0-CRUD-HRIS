3f5949f17f714111812437c6c141a4c4d4f10ece
Netkee <kenneth.regonay@gmail.com>
2021-01-07 23:18:27 +0800

    the code for the nav/header of the website which i separate in the index.php or to the main website.

---
+++ src/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!-- navigation bar-->
+<nav>
+    <div class="logo">
+        <a href="index.php"><h4><i class="fas fa-fingerprint"></i>HRIS</h4></a>
+    </div>
+    <ul class="nav-links">
+          <li>
+            <a href="#" class="href"><i class="fas fa-address-card"></i>EMPLOYEE</a>
+        </li>
+         <li>
+            <a href="#" class="href"><i class="far fa-id-badge"></i>POSITION</a>
+        </li>
+          <li>
+            <a href="#" class="href"><i class="far fa-building"></i>DEPARTMENT</a>
+        </li>
+    </ul>
+    <div class="mobile-view">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+</nav>
+</html>
