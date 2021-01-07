54929bc2417dcaffad9962479bbcbb9bb910bfe8
Netkee <kenneth.regonay@gmail.com>
2021-01-07 23:17:27 +0800

    the html for the footer that separated from the index.php/html

---
+++ src/footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!---Footer-->
+<footer> 
+        <div class="container">
+        <div class="socials">
+           <a href="https://web.facebook.com/Netkee"> <i class="fab fa-facebook"></i> Facebook   </a>
+            <a href="https://github.com/kennethregonay/M0-CRUD"><i class="fab fa-github"></i>  Github  </a>
+            <a href="https://www.instagram.com/netkeee/"><i class="fab fa-instagram-square"></i>  Instagram  </a>
+        </div>
+            <div class="copyright">
+                <p> &copy; 2021 Regonay CRUD App </p> 
+                <p>All Rights Reserved.</p>
+            </p>
+            </div>
+        </div>
+    </footer>
+    </html>
