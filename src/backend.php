c9fd061744bc0ea451e383314b1f733e263b1166
Netkee <kenneth.regonay@gmail.com>
2021-01-08 14:18:03 +0800

    link the new file to the backend.php

--- src/backend.php
+++ src/backend.php
@@ -10,40 +10,25 @@
         $lname = $_POST ['lname']; 
         $address = $_POST ['address']; 
         if($_POST ['gender']== 'male'){
-            $gender = 'M'; 
+            $gdr = 'M'; 
         }else{
-            $gender = 'F'; 
+            $gdr = 'F'; 
         }
         
         $bdate = $_POST ['bdate'];  
         $poscode = $_POST ['poscode'];  
           $connection->query("INSERT INTO employee(Firstname,Middlename,Lastname,Address,Gender,Birthdate,Position_Code) 
-        VALUES ('$fname','$mname','$lname','$address','$gender','$bdate','$poscode')")
+        VALUES ('$fname','$mname','$lname','$address','$gdr','$bdate','$poscode')")
         or die ($connection->error);
         prompt(".success","Record is Successfully Saved!");
     }
     if (isset($_GET['delete'])){
         $id = $_GET['delete'];
-          $connection->query("DELETE FROM employee WHERE EmployeeID = $id")
+          $connection->query("DELETE FROM employee WHERE EmployeeID = '$id'")
         or die ($connection->error);
         prompt(".Delete","Record is Successfully Deleted!");
     }
 
-    if (isset($_POST['update']))  {
-       $fname = $_POST ['fname'];
-        $mname = $_POST ['mname']; 
-        $lname = $_POST ['lname']; 
-        $address = $_POST ['address']; 
-        if($_POST ['gender']== 'male'){
-            $gender = 'M'; 
-        }else{
-            $gender = 'F'; 
-        }
-        
-        $bdate = $_POST ['bdate'];  
-        $poscode = $_POST ['poscode']; 
-        $id = $GET['id']; 
-    }  
     function prompt($class,$msg){
         $element = "<h5 class='$class'>$msg</h5>";
         echo $element;
